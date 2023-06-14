616eb5191a6a6f15192a2766cc57c27178591c9b
anikaaurony <aurony111@gmail.com>
2023-06-14 20:33:11 +0600

    cart booked commit

--- index.js
+++ index.js
@@ -142,8 +142,8 @@ async function run() {
         });
 
         app.delete('/carts/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
+            const id = req.params.id;           
+            const query = { _id: id };       
             const result = await cartCollection.deleteOne(query);
             res.send(result);
           })
